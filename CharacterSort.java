2da80dae935acac1a95c9a590a4da49ceba259cb
Mr. Dhananjaya Sahoo <dhananjaya2608@gmail.com>
2026-06-14 00:07:56 +0530

    Add files via upload

---
+++ CharacterSort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class CharacterSort {
+    public static void main(String[] args) {
+        char arr[] = {'R', 'A', 'C', 'D', 'B'};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
